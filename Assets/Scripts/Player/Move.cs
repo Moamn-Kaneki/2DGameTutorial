dcf5d850337670507390b6257664374a59cb9f3e
Moamn-Kaneki <moamnelfnan@gmail.com>
2024-03-02 21:45:08 +0200

    Run animation and facing direction

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {  
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        if(direction != 0)
+        {
+            animator.SetBool("Run",true);
+        } 
+        else
+        {
+            animator.SetBool("Run",false);
+        }
+        if(direction < 0)
+        {
+            transform.rotation = new Quaternion(0,180,0,0);
+        }
+        else if(direction > 0 )
+        {
+            transform.rotation = new Quaternion(0,0,0,0);
+        }
+        transform.position += new Vector3(direction*Time.deltaTime*3, 0, 0);
+    }
+}
